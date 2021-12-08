395fa924266cca2e678ac0e1520f7c05ef312483
Денис <denis@ip-192-168-0-100.is74.loc>
2021-12-08 20:10:36 +0500

    Изменил сортировку таблицы

--- include/getTable.php
+++ include/getTable.php
@@ -4,25 +4,5 @@ require_once $_SERVER['DOCUMENT_ROOT'] . '/include/connect.php';
 
 function getTable($pdo)
 {
-    return $pdo->query('SELECT * FROM brackets')->fetchAll(PDO::FETCH_ASSOC);
+    return $pdo->query('SELECT * FROM brackets ORDER BY id DESC')->fetchAll(PDO::FETCH_ASSOC);
 }
-
-//
-//echo json_encode(['table' => '<table class="table">
-//            <thead>
-//                <tr>
-//                    <th scope="col">Выражение</th>
-//                    <th scope="col">Результат</th>
-//                </tr>
-//            </thead>
-//            <tbody>
-//                <tr>
-//                    <td>Mark</td>
-//                    <td>Otto</td>
-//                </tr>
-//                <tr>
-//                    <td>1</td>
-//                    <td>Введите выражение</td>
-//                </tr>
-//            </tbody>
-//        </table>']);
